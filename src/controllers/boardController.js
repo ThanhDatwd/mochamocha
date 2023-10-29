cf2d326e7fd86483153c269356214d08955f86e2
nguyenthanhdat <87852737+ThanhDatwd@users.noreply.github.com>
2023-10-29 14:16:52 +0700

    test commit

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -6,7 +6,6 @@ import { boardService } from "~/services/boardService";
 const createNew = async (req, res, next) => {
   try {
     const result = await boardService.createNew(req.body);
-
     res.status(StatusCodes.CREATED).json(result);
   } catch (error) {
     next(error);
